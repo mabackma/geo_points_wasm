6caf8b58c9b297cc22ef8b0f706e9311f2635c57
Marc Bäckman <93243973+mabackma@users.noreply.github.com>
2024-09-13 00:33:48 +0300

    geojson function now uses coordinates

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use geo_points::main_functions::{
-    get_bounding_box_of_map, 
+    create_geo_json_from_coords, 
+    draw_and_save_selected_stand, 
+    draw_stands_in_bbox, get_bounding_box_of_map, 
     random_bbox, 
-    create_geo_json_for_bbox, 
-    save_geojson, 
-    draw_stands_in_bbox, 
-    draw_and_save_selected_stand
+    save_geojson
 };
+use geo_points::geometry_utils::get_min_max_coordinates;
 
 fn main() {
     let mut bbox = get_bounding_box_of_map();
     bbox = random_bbox(&bbox);
 
-    let map_geojson = create_geo_json_for_bbox(&mut bbox);
+    let(min_x, max_x, min_y, max_y) = get_min_max_coordinates(&bbox);
+
+    let map_geojson = create_geo_json_from_coords(min_x, max_x, min_y, max_y);
     let filename = "stands_in_bbox.geojson";
     save_geojson(&map_geojson, filename);
 
