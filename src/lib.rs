537941b5e93e1c10bfce70c6d398bf93ebc6abae
Marc Bäckman <93243973+mabackma@users.noreply.github.com>
2024-09-19 09:55:25 +0300

    removed unnecessary imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,2 @@
-use geo::BooleanOps;
-use geo_points::forest_property::forest_property_data::ForestPropertyData;
-use geo_points::main_functions::{
-    //create_geo_json_from_coords, 
-    draw_selected_stand, 
-    draw_stands_in_bbox, 
-    get_bounding_box_of_map, 
-    random_bbox, 
-    save_geojson
-};
-use geo_points::geometry_utils::get_min_max_coordinates;
-//use geo_points::requests_wasm::{fetch_buildings, fetch_roads};
 use geo_points::requests_wasm::geo_json_from_coords;
-use geojson::{FeatureCollection, GeoJson};
-use std::error::Error;
 
