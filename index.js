3103110189cd0d18c8045ad3450bed6d0c70b66c
Marc Bäckman <93243973+mabackma@users.noreply.github.com>
2024-09-26 03:03:54 +0300

    logging trees in console

--- index.js
+++ index.js
@@ -15,10 +15,16 @@ async function handleFile(file) {
             const start = performance.now();
 
             // Example coordinates for the bounding box
+            /*
             const min_x = 25.347136232586948;
             const max_x = 25.42651387476916;
             const min_y = 66.42980392068148;
             const max_y = 66.46850960846054;
+            */
+            const min_x = 25.37992580430022;
+            const max_x = 25.39148329862563;
+            const min_y = 66.4536396062254;
+            const max_y = 66.45570515162402;
 
             // Call the WebAssembly function with the XML content
             const result = await geo_json_from_coords(min_x, max_x, min_y, max_y, xmlContent);
@@ -30,7 +36,7 @@ async function handleFile(file) {
             const buffer = new SharedBuffer(maxTreeCount);
 
             // Access the raw memory buffer directly using Float64Array
-            const wasmMemory = new Float64Array(memory.buffer, buffer.ptr(), buffer.len());
+            const wasmMemory = new Float64Array(memory.buffer, buffer.ptr(), maxTreeCount * 3);
 
             // End timing
             const end = performance.now();
@@ -41,12 +47,19 @@ async function handleFile(file) {
             console.log('GeoJson:', geojson);
             console.log('Max tree count:', maxTreeCount);
             console.log('Tree count:', treeCount);
+            console.log('Buffer Pointer:', buffer.ptr());
+
             for (let i = 0; i < wasmMemory.length; i += 3) {
                 const x = wasmMemory[i];      // x coordinate
                 const y = wasmMemory[i + 1];  // y coordinate
                 const species = wasmMemory[i + 2]; // species as f64
-                console.log(`Tree: x=${x}, y=${y}, tree species=${species}`);
+
+                if (species !== 0) {
+                    console.log(`JAVASCRIPT Tree: x=${x}, y=${y}, tree species=${species}`);
+                }                
             }
+
+            console.log(`Done logging ${wasmMemory.length / 3} trees`);
         } catch (error) {
             console.error('Error:', error);
         }
